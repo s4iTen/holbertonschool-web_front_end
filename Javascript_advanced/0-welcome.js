a6f867bc71aab9464df65336943965a281f726a3
s4iTen <ychouk102@gmail.com>
2024-01-27 13:28:01 +0100

    this is the js function that alert some strings

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,8 @@
+function welcome(firstName, lastName) {
+ const fullName = firstName+ ' ' + lastName;
+    function displayFullName(fullName) {
+        alert("Welcome " + fullName + '!');
+    }
+    displayFullName(fullName);
+}
+welcome();
