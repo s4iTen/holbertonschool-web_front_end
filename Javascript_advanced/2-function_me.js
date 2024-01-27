eefaf40349e2e4707f090349e79edb2e63b6499e
s4iTen <ychouk102@gmail.com>
2024-01-27 13:43:25 +0100

    this is the closure of js

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,6 @@
+function welcomeMessage(fullName) {
+    return alert('Welcome ' + fullName);
+}
+const guillaume = welcomeMessage('Guillaume')
+const alex = welcomeMessage('Alex')
+const fred = welcomeMessage('Fred')
