d7de65a62cf4dc76b30ac96f5744bb56e1046cd9
s4iTen <ychouk102@gmail.com>
2024-01-29 13:50:53 +0100

    this is the last updates of the prime num function

--- Javascript_advanced/10-prime.js
+++ Javascript_advanced/10-prime.js
@@ -1,31 +1,28 @@
 function countPrimeNumbers() {
-  function isPrime(num) {
-    if (num <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) return false;
+    function isPrime(num) {
+      if (num <= 1) return false;
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+      }
+      return true;
     }
-    return true;
-  }
-
-  let count = 0;
-  for (let i = 2; i <= 100; i++) {
-    if (isPrime(i)) {
-      count++;
+  
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+      if (isPrime(i)) {
+        count++;
+      }
     }
+  
+    return count;
   }
-
-  return count;
-}
-const startTime = performance.now();
-setTimeout(() => {
+  const startTime = performance.now();
   for (let i = 0; i < 100; i++) {
-    var primenum = countPrimeNumbers();
+      countPrimeNumbers();
   }
-  return primenum;
-}, 0);
-const endTime = performance.now();
-const executionTime = endTime - startTime;
-
-console.log(
-  `Execution time of counting prime numbers was ${executionTime} milliseconds.`
-);
+  const endTime = performance.now();
+  const executionTime = endTime - startTime;
+  console.log(
+    `Execution time of counting prime numbers was ${executionTime} milliseconds.`
+  );
+  
